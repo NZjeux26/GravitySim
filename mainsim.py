bb5305652c61b41ccc16770ce1ab8405ed2854a8
NZjeux26 <91103932+NZjeux26@users.noreply.github.com>
2024-05-15 22:15:07 +1200

    added cal force

--- mainsim.py
+++ mainsim.py
@@ -10,6 +10,16 @@ window_size = (800, 600)
 window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Pygame Window")
 
+#drawn objects
+
+circle_radius = 50
+c1_pos = (200,300)
+c2_pos = (600, 300)
+
+
+clock = pygame.time.Clock()
+fps = 60
+
 # Main loop
 running = True
 while running:
@@ -21,7 +31,9 @@ while running:
     # Update
 
     # Draw
-
+    window.fill(255,255,255)
+    pygame.draw.circle(window, 255,0,0, c1_pos, circle_radius)
+    pygame.draw.circle(window, 0,255,0, c2_pos, circle_radius)
     # Refresh display
     pygame.display.flip()
 
--- objects.py
+++ objects.py
@@ -3,6 +3,9 @@ import time
 import numpy
 import array
 
+class Constants:
+    gravitational_constant = 6.6740e-11
+
 class PointMass:
     def __init__(self, xpos, ypos, zpos, mass, radius, colour, velocity=array([0, 0, 0]), acceleration=array([0,0,0])):
         self.xpos = xpos
@@ -13,4 +16,7 @@ class PointMass:
         self.colour = colour
         self.velocity = velocity
         self.acceleration = acceleration
+    def cal_gForce(self,mass1, mass2, distance):
+        return  Constants.gravitational_constant * (mass1 * mass2) / distance^2
+    
         
